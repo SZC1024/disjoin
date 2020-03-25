44098cb3bdf59b8b81c95e7cf26992e6c61f052f
妖精梦梦 <1054550112@qq.com>
2020-03-25 18:54:38 +0800

    module.mk

--- Slave/module.mk
+++ Slave/module.mk
@@ -1,5 +1,5 @@
 local_src := $(wildcard $(subdirectory)/*.cpp)
 
-$(eval $(call make-program,disjoin-slave,libtriplebit.a,$(local_src)))
+$(eval $(call make-program,startslave,libtriplebit.a,$(local_src)))
 
 $(eval $(call compile-rules))
